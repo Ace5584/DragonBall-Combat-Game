d501b2fec85dedc1bdbc7cebceb25dd9635cc20a
Alex <aexace666@gmail.com>
2020-06-19 07:28:51 +1000

    Created hibox and successfully fixed bugs including flickering when jumping, and p1 not moving animation not moving

--- Pygame/L1/main.py
+++ Pygame/L1/main.py
@@ -27,15 +27,12 @@ for x in range(4):
     walkLeft = walkLeft + walkLeft
     c2_walkRight = c2_walkRight + c2_walkRight
     c2_walkLeft = c2_walkLeft + c2_walkLeft
-    print("A")
+
 bg = pygame.image.load('bg.jpg')
 
 
 class player():
     def __init__(self, x, y, width, height, left, text_location, player_name):
-        font = pygame.font.Font('freesansbold.ttf', 18)
-        self.player_information = font.render(player_name, False, (255, 255, 255))
-        self.text_location = text_location
         self.x = x
         self.y = y
         self.width = width
@@ -53,7 +50,15 @@ class player():
         self.bullets = list()
         self.bullet_count = 10
         self.walking = bool()
-        self.hitbox = (self.x+20, self.y, 28, 60)
+        self.hit_box = (self.x + 20, self.y, 28, 60)
+        self.font = pygame.font.Font('freesansbold.ttf', 18)
+        self.text_location = text_location
+        self.player_information = self.font.render(player_name, False, (255, 255, 255))
+        self.lst = list(text_location)
+        self.lst[1] += 20
+        self.bullet_count_text_location = tuple(self.lst)
+        self.player_bullet_count = self.font.render(f'Bullet Remaining: {self.bullet_count}', False, (255, 255, 255))
+        print(self.bullet_count)
 
     def draw(self, win, character):
         if character == 1:
@@ -61,32 +66,92 @@ class player():
                 if self.walk_count + 1 >= len(walkLeft):
                     self.walk_count = 0
                 elif self.left:
-                    win.blit(walkLeft[round(self.walk_count // 3)], (self.x, self.y))
+                    win.blit(walkLeft[int(round(self.walk_count // 3))], (int(self.x), int(self.y)))
                     self.walk_count += 1
                 elif self.right:
-                    win.blit(walkRight[round(self.walk_count // 3)], (self.x, self.y))
+                    win.blit(walkRight[int(round(self.walk_count // 3))], (int(self.x), int(self.y)))
                     self.walk_count += 1
             else:
                 if self.left:
                     win.blit(walkLeft[0], (int(self.x), int(self.y)))
                 else:
                     win.blit(walkRight[0], (int(self.x), int(self.y)))
-        elif character == 2:
+            self.hit_box = (self.x + 17, self.y + 11, 29, 52)
+            pygame.draw.rect(window, (255, 0, 0), self.hit_box, 2)
+        if character == 2:
             if p2.walking:
                 if self.walk_count + 1 >= len(c2_walkLeft):
                     self.walk_count = 0
                 elif self.left:
-                    win.blit(c2_walkLeft[round(self.walk_count // 3)], (self.x, self.y))
+                    win.blit(c2_walkLeft[round(self.walk_count // 3)], (int(self.x), int(self.y)))
                     self.walk_count += 1
                 elif self.right:
-                    win.blit(c2_walkRight[round(self.walk_count // 3)], (self.x, self.y))
+                    win.blit(c2_walkRight[round(self.walk_count // 3)], (int(self.x), int(self.y)))
                     self.walk_count += 1
             else:
                 if self.left:
                     win.blit(c2_walkLeft[0], (int(self.x), int(self.y)))
                 else:
                     win.blit(c2_walkRight[0], (int(self.x), int(self.y)))
+            self.hit_box = (self.x + 17, self.y + 2, 31, 57)
+            pygame.draw.rect(window, (255, 0, 0), self.hit_box, 2)
 
+    def movement(self, wasd):
+        for bullet in self.bullets:
+            if 0 < bullet.x < screen_x:
+                bullet.x += bullet.vol
+            else:
+                self.bullets.pop(self.bullets.index(bullet))
+        keys = pygame.key.get_pressed()
+        if wasd:
+            key_shoot = keys[pygame.K_q]
+            key_left = keys[pygame.K_a]
+            key_right = keys[pygame.K_d]
+            key_up = keys[pygame.K_w]
+        else:
+            key_shoot = keys[pygame.K_SPACE]
+            key_left = keys[pygame.K_LEFT]
+            key_right = keys[pygame.K_RIGHT]
+            key_up = keys[pygame.K_UP]
+
+        if key_shoot:
+            if self.bullet_count > 0:
+                if self.left:
+                    shooting_direction = -1
+                else:
+                    shooting_direction = 1
+                self.bullets.append(
+                projectile(round(self.x + self.width / 2), round(self.y + self.height / 2), shooting_direction))
+                self.bullet_count -= 1
+                self.player_bullet_count = self.font.render(f'Bullet Remaining: {self.bullet_count}', False, (255, 255, 255))
+
+        if key_left and self.x > self.vol:
+            self.x -= self.vol
+            self.left = True
+            self.right = False
+            self.walking = True
+        elif key_right and self.x < screen_x - self.vol - self.width:
+            self.x += self.vol
+            self.left = False
+            self.right = True
+            self.walking = True
+        else:
+            self.walking = False
+            self.walk_count = 0
+        if not self.jump:
+            if key_up:
+                self.jump = True
+                self.walk_count = 0
+        else:
+            if self.jump_count >= -10:
+                n = 1
+                if self.jump_count < 0:
+                    n = -1
+                self.y -= self.jump_count ** 2 / 2 * n
+                self.jump_count -= 1
+            else:
+                self.jump = False
+                self.jump_count = 10
 
 class projectile():
     def __init__(self, x, y, direction):
@@ -99,63 +164,6 @@ class projectile():
         pygame.draw.circle(win, color, (self.x, self.y), 5)
 
 
-def movement(player, wasd):
-    global bullet_count
-    shooting_direction = int()
-    for bullet in player.bullets:
-        if 0 < bullet.x < screen_x:
-            bullet.x += bullet.vol
-        else:
-            player.bullets.pop(player.bullets.index(bullet))
-    keys = pygame.key.get_pressed()
-    if wasd:
-        key_shoot = keys[pygame.K_q]
-        key_left = keys[pygame.K_a]
-        key_right = keys[pygame.K_d]
-        key_up = keys[pygame.K_w]
-    else:
-        key_shoot = keys[pygame.K_SPACE]
-        key_left = keys[pygame.K_LEFT]
-        key_right = keys[pygame.K_RIGHT]
-        key_up = keys[pygame.K_UP]
-
-    if key_shoot:
-        if player.left:
-            shooting_direction = -1
-        else:
-            shooting_direction = 1
-
-        player.bullets.append(projectile(round(player.x + player.width / 2), round(player.y + player.height / 2), shooting_direction))
-
-    if key_left and player.x > player.vol:
-        player.x -= player.vol
-        player.left = True
-        player.right = False
-        walking = True
-    elif key_right and player.x < screen_x - player.vol - player.width:
-        player.x += player.vol
-        player.left = False
-        player.right = True
-        walking = True
-    else:
-        walking = False
-        p1.walk_count = 0
-    if not player.jump:
-        if key_up:
-            player.jump = True
-            player.right = False
-            player.left = False
-            p1.walk_count = 0
-    else:
-        if player.jump_count >= -10:
-            n = 1
-            if player.jump_count < 0:
-                n = -1
-            player.y -= player.jump_count ** 2 / 2 * n
-            player.jump_count -= 1
-        else:
-            player.jump = False
-            player.jump_count = 10
 
 
 def redraw_window():
@@ -163,6 +171,8 @@ def redraw_window():
     window.blit(bg, (0, 0))
     window.blit(p1.player_information, p1.text_location)
     window.blit(p2.player_information, p2.text_location)
+    window.blit(p1.player_bullet_count, p1.bullet_count_text_location)
+    window.blit(p2.player_bullet_count, p2.bullet_count_text_location)
     p1.draw(window, 1)
     p2.draw(window, 2)
     for z in p1.bullets:
@@ -172,9 +182,8 @@ def redraw_window():
     pygame.display.update()
 
 
-p1 = player(50, 650, 64, 64, False, (0, 0), "Player 1:")
-p2 = player(1200, 650, 64, 64, True, (1200, 0), "Player 2:")
-bullet_count = 0
+p1 = player(50, 650, 64, 64, False, (0, 0), f'Player 1:')
+p2 = player(1200, 650, 64, 64, True, (1050, 0), "Player 2:")
 
 run = True
 while run:
@@ -182,8 +191,8 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    movement(p1, True)
-    movement(p2, False)
+    p1.movement(True)
+    p2.movement(False)
     redraw_window()
 
 pygame.quit()
